46ac9c641c05b0db80510c2b73baf217dd214571
mattemutti <mattemutti@gmail.com>
2024-03-19 15:09:00 +0100

    add axios request for random mail

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,17 @@ const { createApp } = Vue
 createApp({
 	data() {
 		return {
-			message: 'gasom?'
+			mailRandom: null,
 		}
+	},
+	mounted() {
+		axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+		.then((response) => {
+			console.log(response);
+			this.mailRandom = response.data.response
+			console.log(this.mailRandom);
+		})
+
+
 	}
 }).mount('#app')
