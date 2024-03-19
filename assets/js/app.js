382a0614770d1cdae8f97df857e23d4e6e446ddc
mattemutti <mattemutti@gmail.com>
2024-03-19 17:50:17 +0100

    update correction by Giuliano

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,22 +4,18 @@ createApp({
 	data() {
 		return {
 			userMail: [],
-			
-
 		}
 	},
 	methods: {
 		genMail() {
 			for (let i = 0; i < 10; i++) {
-				console.log(i);
-				if (i <= 10) {
-					axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-						.then((response) => {
-							console.log(response);
-							this.userMail.push(response.data.response);
-						})
-					console.log(i);
-				}
+				//console.log(i);				
+				axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+					.then((response) => {
+						console.log(response);
+						this.userMail.push(response.data.response);
+					})
+				//console.log(i);
 			}
 			console.log(this.userMail);
 		}
